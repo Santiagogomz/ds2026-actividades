b534bc12e1d2eae81330f03eb5dd443a54bb89b8
Santiagogomz <sa.gomez@alu.frlp.utn.edu.ar>
2026-04-16 14:46:20 -0300

    actualizacion

---
+++ c03/Ejercicio1-Variables-y-operaciones/app.js
@@ -0,0 +1,12 @@
+const nombre= 'Santiago';
+const edad= 20;
+const materia_en_curso= 'Desarrollo de Software';
+console.log(`Me llamo ${nombre} , tengo ${edad} años y curso ${materia_en_curso}`)
+
+let contador = 0;
+
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+console.log(`Contador:${contador}`)
