ab0522a2af17cf1557b4c66ade45826299c60537
Santiagogomz <sa.gomez@alu.frlp.utn.edu.ar>
2026-04-17 11:49:29 -0300

    actualización carpeta

---
+++ c03/Ejercicio2—Condicionales/app.js
@@ -0,0 +1,94 @@
+let resultado;
+let dia;
+function verAprobado(){
+
+let nota = Number(prompt("Ingrese nota:"));
+
+if (nota < 4)
+{
+
+    resultado = "Desaprobado";
+}
+else if( nota >= 4 && nota <= 7 )
+{
+
+    resultado = "Aprobado";
+
+}
+else
+{
+
+    resultado = "Promocionado";
+
+}
+    return resultado;
+}
+
+
+console.log(verAprobado());
+
+function diaDeLaSemana()
+{
+
+    let dia = Number(prompt("Ingrese un número para retornar el día correspondiente:"));
+
+    switch(dia)
+    {
+
+        case 1: 
+
+            console.log("1: Lunes");
+
+            break;
+
+        case 2:
+
+            console.log("2: Martes");
+
+            break;
+
+        case 2:
+
+            console.log("2: Martes");
+
+            break;
+
+        case 3:
+
+            console.log("3: Miercoles");
+
+            break;
+
+        case 4:
+
+            console.log("4: Jueves");
+
+            break;
+
+        case 5:
+
+            console.log("5: Viernes");
+
+            break;
+
+        case 6:
+
+            console.log("6: Sábado");
+
+            break;
+
+        case 7:
+
+            console.log("7: Domigo");
+
+            break;
+
+        default:
+
+            console.log("Día inválido");
+
+            
+    }
+
+}
+
